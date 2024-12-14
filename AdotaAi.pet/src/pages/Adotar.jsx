658fcb86d13a50e263bfe6b6aa17b9c633f2ffbd
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2024-12-14 15:26:18 -0300

    ajustado os cards dos pets

--- AdotaAi.pet/src/pages/Adotar.jsx
+++ AdotaAi.pet/src/pages/Adotar.jsx
@@ -42,7 +42,9 @@ function Adotar(){
     <div className={styles.gallery}>
 
             <div  className={styles.card}>
+            <div className={styles.imagem}>
                 <img src="https://images.unsplash.com/photo-1503256207526-0d5d80fa2f47?q=80&w=1372&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt=""/>
+            </div>
                 <div className={styles.infos}>
                     <h1 className={styles.nome}><a href="">Bob</a> <img src="imagens/macho (1).png" alt=""/></h1> 
                     
@@ -51,16 +53,21 @@ function Adotar(){
             </div>
 
             <div  className={styles.card}>
+                <div className={styles.imagem}>
                 <img src="https://images.unsplash.com/photo-1422565096762-bdb997a56a84?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt=""/>
-                
+                </div>
                 <div className={styles.infos}>
-                    <h1 className={styles.nome}><a href="">Mel</a></h1> 
+                    <h1 className={styles.nome}><a href="">Mel</a> <img src="imagens/macho (1).png" alt=""/></h1> 
+                    
                     <p>Santos, São Paulo</p>
                 </div>
             </div>
 
+
             <div  className={styles.card}>
+            <div className={styles.imagem}>
                 <img src="https://images.unsplash.com/photo-1517423440428-a5a00ad493e8?q=80&w=1349&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt=""/>
+            </div>
                 <div className={styles.infos}>
                     <h1 className={styles.nome}><a href="">Feijão</a></h1> 
                     <p>Santos, São Paulo</p>
@@ -68,7 +75,9 @@ function Adotar(){
             </div>
 
             <div  className={styles.card}>
+            <div className={styles.imagem}>
                 <img src="https://images.unsplash.com/photo-1530281700549-e82e7bf110d6?q=80&w=1376&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt=""/>
+            </div>
                 <div className={styles.infos}>
                     <h1 className={styles.nome}><a href="">Joaquim</a></h1> 
                     <p>Santos, São Paulo</p>
