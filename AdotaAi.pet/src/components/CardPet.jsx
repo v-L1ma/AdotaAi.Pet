d1082f20ffe5871fce95a0fc83787dadcaf96965
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2024-12-19 22:51:39 -0300

    conexão com a api

--- AdotaAi.pet/src/components/CardPet.jsx
+++ AdotaAi.pet/src/components/CardPet.jsx
@@ -2,14 +2,14 @@ import styles from './CardPet.module.css'
 
 function CardPet({animais}){
     return(
-        <div  className={styles.card} key={styles.id}>
+        <div  className={styles.card} key={animais.id}>
                     <div className={styles.imagem}>
                         <img src={animais.foto} alt=""/>
                     </div>
                         <div className={styles.infos}>
                             <h1 className={styles.nome}><a href="">{animais.nome}</a></h1> 
                             
-                            <p>{animais.loc}</p>
+                            <p>{animais.localizacao}</p>
                         </div>
                     </div>
     )
--- AdotaAi.pet/src/pages/Adotar.jsx
+++ AdotaAi.pet/src/pages/Adotar.jsx
@@ -1,41 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CardPet from "../components/CardPet";
 import styles from "./Adotar.module.css";
+import api from "../sevices/api";
 
 function Adotar() {
+  const [animais, setAnimais] = useState([])
 
-    const [animais, setAnimais] = useState([
-        {
-            id: 1,
-            foto: "https://images.unsplash.com/photo-1503256207526-0d5d80fa2f47?q=80&w=1372&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            nome: "Bob",
-            loc: "Santos - SP",
-          },
-          {
-            id: 2,
-            foto: "https://images.unsplash.com/photo-1422565096762-bdb997a56a84?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            nome: "Mel",
-            loc: "Santos - SP",
-          },
-          {
-            id: 3,
-            foto: "https://images.unsplash.com/photo-1517423440428-a5a00ad493e8?q=80&w=1349&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            nome: "Feijão",
-            loc: "Santos - SP",
-          },
-          {
-            id: 4,
-            foto: "https://images.unsplash.com/photo-1530281700549-e82e7bf110d6?q=80&w=1376&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            nome: "Joaquim",
-            loc: "Santos - SP",
-          },
-          {
-            id: 5,
-            foto: "https://www.petz.com.br/blog/wp-content/uploads/2019/07/vida-de-gato.jpg",
-            nome: "Felpudo",
-            loc: "Santos - SP",
-          },
-    ])
+  async function listaAnimais(){
+    const animaisFromApi = await api.get('/animais')
+    setAnimais(animaisFromApi.data)
+  }
+
+  useEffect(() => {
+
+    listaAnimais()
+
+
+
+  }, [])
 
   return (
     <main>
@@ -73,15 +55,10 @@ function Adotar() {
         </div>
 
         <div className={styles.gallery}>
-
-          {
-          animais.map((animais) => (
-            <CardPet animais={animais}/>
-          ))
-          }
-
+          {animais.map((animais) => (
+            <CardPet animais={animais} />
+          ))}
         </div>
-
       </div>
     </main>
   );
