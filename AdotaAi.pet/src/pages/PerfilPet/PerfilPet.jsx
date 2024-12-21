4ed1a01ca4e12c00842e09534384fbcd3ef934c1
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2024-12-21 13:54:09 -0300

    Atualização nos estilos

--- AdotaAi.pet/src/pages/PerfilPet/PerfilPet.jsx
+++ AdotaAi.pet/src/pages/PerfilPet/PerfilPet.jsx
@@ -6,14 +6,22 @@ function PerfilPet(){
         <main>
             <div className={styles.container}>
                 <img src="" alt="" />
-                <div className={StyleSheet.info}>
+                <div className={styles.sobre}>
                     <h1>Nome do pet</h1>  
-                    <p><FaLocationDot/>Santos, SP</p>
-                    <h2>Informações do pet</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus fuga hic impedit! Adipisci, omnis a maiores officiis beatae, veritatis nemo dolores iusto provident vitae earum ratione. Officia amet excepturi porro.</p>
-                    <p>Raca:</p>
-                    <p>Idade:</p>
+                    <p className={styles.loc}><FaLocationDot/>Santos, SP</p>
                     
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus fuga hic impedit! Adipisci, omnis a maiores officiis beatae, veritatis nemo dolores iusto provident vitae earum ratione. Officia amet excepturi porro.</p>
+                    <h2>Informações do pet</h2>
+                    <div className={styles.infos}>
+                        <p>Raca:</p>
+                        <p>Idade:</p>
+                        <p>Sexo:</p>
+                        <p>Vacinado:</p>
+                        <p>Castrado:</p>
+                        <p>Vermifugado:</p>
+                        <p></p>
+                        <p></p>
+                    </div>
                 </div>
             </div>
         </main>
@@ -21,15 +29,3 @@ function PerfilPet(){
 }
 
 export default PerfilPet
-
-/*
-  nome  String
-  raca  String
-  foto  String
-  datanasc String
-  sexo String
-  vacinado String
-  castrado String
-  vermifugado String
-  descricao String
-*/
